9ac6d8ceac3e55242769ac89e1f1d846f98f0115
Otto <ottohallemail@gmail.com>
2026-03-08 20:30:39 +0000

    Add custom OTTO favicon with clock showing dinner time

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Otto - Reservation Bot',
   description: 'Personal Resy automation',
+  icons: {
+    icon: '/favicon.svg',
+  },
 }
 
 export default function RootLayout({
